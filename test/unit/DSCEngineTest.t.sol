89d2d6696e071a33421a916301134099270b723a
Rusty-Metalhead <rustymetalcoder@gmail.com>
2024-03-25 09:52:04 +0530

    WIP: write basic depositCollateral test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,11 +16,16 @@ contract DSCEngineTest is Test {
     HelperConfig config;
     address wethUsdPriceFeed;
     address weth;
+    address immutable i_alice = makeAddr("alice");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
         (wethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(i_alice, STARTING_ERC20_BALANCE);
     }
     ////////////////////
     // PriceFeed Test //
@@ -36,4 +42,12 @@ contract DSCEngineTest is Test {
     /////////////////////////////
     // Deposit Collateral Test //
     /////////////////////////////
+    function testDepositCollateral() public {
+        vm.startPrank(i_alice);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__AmountMustBeMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
 }
